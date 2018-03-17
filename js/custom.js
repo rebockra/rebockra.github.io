9174638bbb28bcc683a2e0c11fa2fbe185987a08
Rene Bockrandt <rbockran@students.uni-mainz.de>
2018-03-17 10:40:37 +0100

    rotation

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('trigger').onclick = function() {
     var herzchen = ' \u2764'
     var herzchenString = "";
@@ -10,12 +11,96 @@ document.getElementById('trigger').onclick = function() {
     document.getElementById("target").appendChild(list);
     list.innerText = herzchenString;
     list.appendChild(listItem);
-    /*
+    
     for (var i = 2; i < 10; i++) {
       var text = "Hallo Mona hier sind " + i + '';
       var listItem = document.createElement("li");
       listItem.innerText = text;
       list.appendChild(listItem);
     }
-    */
+   
   }
+   */
+
+   var toggleImage = false;
+   var showImage = false;
+
+   $(document).ready(function() {
+     $(".translucent").hide();
+   })
+
+   
+   $(".acc-work-title").click(function() {
+     let addon = "fa-spin";
+     var icon = $(".fa-clock");
+        var isSpinning = icon.html().contains(addon);
+        if(isSpinning){
+        icon.removeClass(addon);
+        return;
+      } else {
+        icon.toggleClass(addon);
+        return;
+      }
+     })
+
+     $(".acc-motivation-title").click(function() {
+      let addon = "fa-spin";
+      var icon = $(".fa-lightbulb");
+         var isSpinning = icon.html().contains(addon);
+         if(isSpinning){
+         icon.removeClass(addon);
+         return;
+       } else {
+         icon.toggleClass(addon);
+         return;
+       }
+      })
+     
+
+      $(".acc-resume-title").click(function() {
+        let addon = "fa-spin";
+        var icon = $(".fa-file-alt");
+           var isSpinning = icon.html().contains(addon);
+           if(isSpinning){
+           icon.removeClass(addon);
+           return;
+         } else {
+           icon.toggleClass(addon);
+           return;
+         }
+        })
+
+     /*
+     $(".accordion-title").click(function() {
+      let addon = "fa-spin";
+      var icon = $(this).siblings("h2").firstChild;
+      console.log(icon);
+         var isSpinning = icon.html().contains(addon);
+         if(isSpinning){
+         icon.removeClass(addon);
+         return;
+       } else {
+         icon.toggleClass(addon);
+         return;
+       }
+      })
+      */
+
+     
+    $(".trigger").click(function() {
+      var img = $(".translucent");
+      if (!showImage){
+      img.show();
+      showImage = true;
+      }
+      if(toggleImage){
+        img.animate({height: '0', opacity: '0.4'}, "slow");
+        img.animate({width: '100vw', opacity: '0.4'}, "slow");
+        toggleImage =! toggleImage;
+      } else {
+        img.animate({height: '20vh', opacity: '0.3'}, "slow");
+        img.animate({width: '100vw', opacity: '0.5'}, "slow");
+        toggleImage =! toggleImage;
+      }
+    })
+    
